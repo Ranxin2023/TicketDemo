f6f0690b4befe48098c7ec18a5392506c2ded008
Ranxin Li <ranxinli2024@gmail.com>
2024-07-05 00:19:10 -0700

    first commit

---
+++ reactdemo1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TicketCard  from './App/ticketcard.jsx';
+function App() {
+  return (
+    
+    <div>
+      <TicketCard/>
+      <ResponseCard/>
+    </div>
+  );
+}
+
+export default App;
