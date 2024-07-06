fc6d78bf44053e03e8887f224bff579043b5c164
RanxinLi <rstli@ucdavis.edu>
2024-07-05 19:22:54 -0700

    upload to github

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicketCard  from './App/ticketcard.jsx';
+import ResponseCard from './App/responsecard.jsx';
+function App() {
+  return (
+    
+    <div>
+      <TicketCard/>
+      <ResponseCard/>
+    </div>
+  );
+}
+
+export default App;
