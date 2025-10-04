96922701285a45c34e9c3a9ccd486bd766e5a0a1
RanxinLi <ranxinli2024@gmail.com>
2025-10-04 05:29:23 -0700

    change github email and add gitignore

---
+++ client/src/App/responsecard.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+const ResponseCard=({responseText})=>{
+    return(
+        <div className="response-text">{responseText}</div>
+    )
+}
+export default ResponseCard;
